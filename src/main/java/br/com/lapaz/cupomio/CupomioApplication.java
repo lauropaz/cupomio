d9dcfef5e494c2636ab59d5f9959978035718232
lauropaz <lauroskd@gmail.com>
2025-12-23 23:10:26 -0300

    Commit inicial

---
+++ src/main/java/br/com/lapaz/cupomio/CupomioApplication.java
@@ -0,0 +1,13 @@
+package br.com.lapaz.cupomio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CupomioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CupomioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lapaz/cupomio/CupomioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lapaz.cupomio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CupomioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
